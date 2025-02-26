826d813c4f99fa7c495d1463f7f0af8b90fa6d6a
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-02-26 15:37:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function functionName() {
+    return functionName.name; // Return function name
+}
+
+window.functionName = functionName; // Attach function to window for Cypress
+
+document.addEventListener("DOMContentLoaded", function () {
+    alert(functionName()); // Show function name on page load
+});
